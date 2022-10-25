89b739621a3dd073ffd5b00a857829aaabe39545
Knyazew36 <knyazew.sv@gmail.com>
2022-10-25 23:55:39 +0300

    5

---
+++ js/main_trener.js
@@ -0,0 +1,11 @@
+const array = [1, 2, 3, 4, 5, 6, 7]
+array.forEach((item)=>{
+ console.log(item)
+ for (let index = 0; index < array.length; index++) {
+   if (index !== item) {
+    
+   }
+
+ }
+})
+
