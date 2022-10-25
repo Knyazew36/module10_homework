f5cacbe30af3e9629393d942f02db4c57f58923a
Knyazew36 <knyazew.sv@gmail.com>
2022-10-25 17:31:58 +0300

    1

---
+++ js/main_1.js
@@ -0,0 +1,23 @@
+let a = prompt()
+
+if (a === null) {
+    console.log("Вы отказались от ввода")
+} else {
+    a = +a
+let type = typeof a
+    if (a % 2 ===0) {
+        console.log("четное");
+         
+    }
+    else if (a % 1 ===0) {
+        console.log("нечетное");
+         
+   }
+   else {
+       console.log("Упс, кажется вы ошиблись")
+    }
+}   
+   
+
+
+
