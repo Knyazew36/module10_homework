204da0462b26e27bafcbdc3de7f6b4be4dd246e9
Knyazew36 <knyazew.sv@gmail.com>
2022-10-26 00:13:58 +0300

    commit

--- js/main_trener.js
+++ js/main_trener.js
@@ -1,11 +1,18 @@
-const array = [1, 2, 3, 4, 5, 6, 7]
-array.forEach((item)=>{
- console.log(item)
- for (let index = 0; index < array.length; index++) {
-   if (index !== item) {
-    
-   }
+const array = [7, 7, 3, 4, 5, 6, 7]
+array.forEach((item, index)=>{
+ let n = console.log(item, index)
+ ff(item, index)
 
- }
+
 })
 
+
+function ff(item,index) {
+    a = item[`index`]
+    console.log(a)
+    if(item[index] !== item[index]+1){
+        // console.log('normalno')
+    }else{
+        // console.log('NEnormalno')
+    }
+}
