75ac9a1a5bb6c411ea5983a21fc68fef65a3d679
Knyazew36 <knyazew.sv@gmail.com>
2022-10-26 21:26:33 +0300

    26.10

--- js/main_6.js
+++ js/main_6.js
@@ -1,6 +1,5 @@
 //Задание №6
 
-
 const arr = [1, 2, 2, 3, 4, 5, 6, 7];
 
 
@@ -8,9 +7,12 @@ let isEqual = true
 const reference = arr[0]
 
 for (let item of arr) {
-    if( item !== reference){
+    if ( item !== reference){
         isEqual = false
     }
 }
 
 console.log(isEqual)
+
+
+
